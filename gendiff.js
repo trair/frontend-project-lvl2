491dc7a4b1f14954177be315710d6d68558a3da0
Maxim Nikonov <the_roffler@mail.ru>
2022-08-05 03:32:41 +0300

    add

---
+++ gendiff.js
@@ -0,0 +1,14 @@
+#!/usr/bin/env node
+import genDiff from '../src/index.js'
+import { Command } from 'commander';
+const program = new Command();
+
+program
+  .name('gendiff')
+  .version('1.0.0')  
+  .description('Compares two configuration files and shows a difference.') 
+  .option('-f, --format <type>', 'output format')
+  .arguments('<filepath1> <filepath2>')
+  .action((filepath1, filepath2) => console.log(genDiff(filepath1, filepath2)))
+
+program.parse();
