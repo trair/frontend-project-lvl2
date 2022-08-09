8e9401e0d3bbc1a92806f0d7f52f97f493258476
Maxim Nikonov <the_roffler@mail.ru>
2022-08-09 20:06:22 +0300

    delete and add

--- src/formatters/format.js
+++ src/formatters/format.js
@@ -1,17 +1,16 @@
 import stylish from './stylish.js';
 import plain from './plain.js';
-import json from './json.js';
 
 const chooseStyle = (file, format = 'stylish') => {
   switch (format) {
     case 'plain':
       return plain(file);
     case 'json':
-      return json(file);
+      return JSON.stringify(file);
     case 'stylish':
       return stylish(file);
     default:
-      throw new Error('Error in switch. Try agagin!')
+      throw new Error(`format ${nameOfFormat} is not supported`);
   }
 };
 
--- src/index.js
+++ src/index.js
@@ -1,48 +1,21 @@
 import fs from 'fs';
 import path from 'path';
-import _ from 'lodash';
-import parse from './parses.js';
+import compareData from './compareData.js';
+import chooseParser from './parsers.js';
+import format from './formats/format.js';
 
-const getExtension = (filepath) => path.extname(filepath);
-const getAbsolutePath = (filepath) => path.resolve(filepath);
-const readFileSync = (filepath) => fs.readFileSync(filepath, 'utf-8')
+const getPath = (file) => path.resolve(process.cwd(), file);
+const readFile = (file) => fs.readFileSync(getPath(file), 'utf-8');
+const getFormat = (file) => file.split('.')[1];
 
-const genDiff = (filepath1, filepath2) => {
-    const extention1 = getExtension(filepath1);
-    const extention2 = getExtension(filepath2);
-
-    const parsedFile1 = parse(readFileSync(getAbsolutePath(filepath1)), extention1);
-    const parsedFile2 = parse(readFileSync(getAbsolutePath(filepath2)), extention2);
-
-    const keys = _.union(Object.keys(parsedFile1), Object.keys(parsedFile2));
-    const sortedKeys = _.sortBy(keys);
-
-    const result = sortedKeys
-      .map((key) => {
-        const value1 = parsedFile1[key];
-        const value2 = parsedFile2[key];
-
-        if (key in parsedFile1 && key in parsedFile2) {
-            if (value1 === value2) {
-                return `    ${key}: ${value1}`;
-            }
-
-            return `  - ${key}: ${value1}\n  + ${key}: ${value2}`;
-        }
-
-        if (key in parsedFile1) {
-            return `  - ${key}: ${value1}`;
-        }
-
-        return `  - ${key}: ${value2}`;
-      })
-      .join('\n')
-      .trim();
-
-      const newResult = `{\n  ${result}\n}`;
-      console.log(newResult);
-
-      return newResult;
+const genDiff = (file1, file2, format = 'stylish') => {
+  const readFile1 = readFile(file1);
+  const readFile2 = readFile(file2);
+  const obj1 = chooseParser(readFile1, getFormat(file1));
+  const obj2 = chooseParser(readFile2, getFormat(file2));
+  const tree = compareData(obj1, obj2);
+  
+  return format(tree, format);
 };
 
 export default genDiff;
