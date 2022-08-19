d778760828d35d2d7a6b794b72ff69da8315f8b6
Maxim Nikonov <the_roffler@mail.ru>
2022-08-19 15:05:30 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import fs from 'fs';
 import path from 'path';
 import compareData from './compareData.js';
 import chooseParser from './parsers.js';
-import format from './formats/format.js';
+import format from './formats/index.js';
 
 const getPath = (file) => path.resolve(process.cwd(), '__fixtures__', file);
 const readFile = (file) => fs.readFileSync(getPath(file), 'utf-8');
