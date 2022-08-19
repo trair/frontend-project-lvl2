7975209d2dbaa4f1f9e96e7c3e7169fba3fd2e98
Maxim Nikonov <the_roffler@mail.ru>
2022-08-19 15:15:12 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -4,15 +4,15 @@ import compareData from './compareData.js';
 import chooseParser from './parsers.js';
 import format from './formats/index.js';
 
-const getAbsolutePath = (file) => path.resolve(process.cwd(), '__fixtures__', file);
-const readFile = (file) => fs.readFileSync(getAbsolutePath(file), 'utf-8');
-const getFormat = (file) => file.split('.')[1];
+const getAbsolutePath = (filepath) => path.resolve(process.cwd(), '__fixtures__', filepath);
+const readFile = (filepath) => fs.readFileSync(getAbsolutePath(filepath), 'utf-8');
+const getFormat = (filename) => filename.split('.')[1];
 
-const genDiff = (file1, file2, nameOfFormat = 'stylish') => {
-  const content1 = readFile(file1);
-  const content2 = readFile(file2);
-  const obj1 = chooseParser(content1, getFormat(file1));
-  const obj2 = chooseParser(content2, getFormat(file2));
+const genDiff = (filepath1, filepath2, nameOfFormat = 'stylish') => {
+  const content1 = readFile(filepath1);
+  const content2 = readFile(filepath2);
+  const obj1 = chooseParser(content1, getFormat(filepath1));
+  const obj2 = chooseParser(content2, getFormat(filepath2));
   const tree = compareData(obj1, obj2);
   return format(tree, nameOfFormat);
 };
