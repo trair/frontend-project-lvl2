721c7448ab95143d061613ce808f1aecd0f656c1
Maxim Nikonov <the_roffler@mail.ru>
2022-08-22 12:25:41 +0300

    fix

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -13,36 +13,16 @@ const file1json = getFixturePath('file1.json');
 const file2json = getFixturePath('file2.json');
 const file1yaml = getFixturePath('file1.yaml');
 const file2yml = getFixturePath('file2.yml');
+const fileFormat = [['stylish', stylishResult], ['plain', plainResult], ['json', jsonResult]];
 
 test('result is string', () => {
   expect(typeof genDiff(file1json, file2json, 'stylish')).toEqual('string');
 });
-test('stylish json-json', () => {
-  expect(genDiff(file1json, file2json, 'stylish')).toEqual(stylishResult);
-});
-test('stylish yaml-yml', () => {
-  expect(genDiff(file1yaml, file2yml)).toEqual(stylishResult);
-});
-test('stylish yaml-json', () => {
-  expect(genDiff(file1yaml, file2json)).toEqual(stylishResult);
+
+test.each(fileFormat)('formater %s, file extension json', (format, expected) => {
+  expect(genDiff(file1json, file2json, format)).toEqual(expected);
 });
 
-test('plain json-json', () => {
-  expect(genDiff(file1json, file2json, 'plain')).toEqual(plainResult);
-});
-test('plain yaml-yml', () => {
-  expect(genDiff(file1yaml, file2yml, 'plain')).toEqual(plainResult);
-});
-test('plain yaml-json', () => {
-  expect(genDiff(file1yaml, file2json, 'plain')).toEqual(plainResult);
-});
-
-test('json json-json', () => {
-  expect(genDiff(file1json, file2json, 'json')).toEqual(jsonResult);
-});
-test('json yaml-yml', () => {
-  expect(genDiff(file1yaml, file2yml, 'json')).toEqual(jsonResult);
-});
-test('json yaml-json', () => {
-  expect(genDiff(file1yaml, file2json, 'json')).toEqual(jsonResult);
+test.each(fileFormats)('formater %s, file extension yaml', (format, expected) => {
+  expect(genDiff(file1yaml, file2yml, format)).toEqual(expected);
 });
