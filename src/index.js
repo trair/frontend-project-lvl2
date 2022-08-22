5c8703031c4476be66361cf98ec13299948e97e2
Maxim Nikonov <the_roffler@mail.ru>
2022-08-22 11:36:38 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import fs from 'fs';
 import path from 'path';
 import compareData from './compareData.js';
-import chooseParser from './parsers.js';
+import parse from './parsers.js';
 import format from './formats/index.js';
 
 const getPath = (file) => path.resolve(process.cwd(), file);
@@ -11,8 +11,8 @@ const getFormat = (file) => file.split('.')[1];
 const genDiff = (file1, file2, nameOfFormat = 'stylish') => {
   const content1 = readFile(file1);
   const content2 = readFile(file2);
-  const obj1 = chooseParser(content1, getFormat(file1));
-  const obj2 = chooseParser(content2, getFormat(file2));
+  const obj1 = parse(content1, getFormat(file1));
+  const obj2 = parse(content2, getFormat(file2));
   const buildTree = compareData(obj1, obj2);
   return format(buildTree, nameOfFormat);
 };
