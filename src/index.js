8696392380d8a6c909b1a533bc6ecf7b4b66863b
Maxim Nikonov <the_roffler@mail.ru>
2022-08-22 11:35:12 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ const genDiff = (file1, file2, nameOfFormat = 'stylish') => {
   const content2 = readFile(file2);
   const obj1 = chooseParser(content1, getFormat(file1));
   const obj2 = chooseParser(content2, getFormat(file2));
-  const tree = compareData(obj1, obj2);
-  return format(tree, nameOfFormat);
+  const buildTree = compareData(obj1, obj2);
+  return format(buildTree, nameOfFormat);
 };
 export default genDiff;
