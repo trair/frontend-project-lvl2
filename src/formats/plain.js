b7bc64b5202060acf6a151c5ccd28b8c7ffad7f9
Maxim Nikonov <the_roffler@mail.ru>
2022-08-24 21:32:25 +0300

    fix

--- src/formats/plain.js
+++ src/formats/plain.js
@@ -34,7 +34,7 @@ const plain = (data) => {
         throw new Error(`Error. Unknown type ${type}!`);
     }
   };
-  const result = data.map((item) => _.compact(iter(item)));
-  return `${result.join('\n')}`;
+  const result = data.map((item) => (iter(item)));
+  return _compact(`${result.join('\n')}`);
 };
 export default plain;
