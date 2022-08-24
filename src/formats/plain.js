7c579ef360b463e84c8b6cdafa660d42ad5833a1
Maxim Nikonov <the_roffler@mail.ru>
2022-08-24 21:19:56 +0300

    fix

--- src/formats/plain.js
+++ src/formats/plain.js
@@ -25,7 +25,7 @@ const plain = (data) => {
       case 'deleted':
         return `Property '${parent}${key}' was removed`;
       case 'unchanged':
-        break;
+        return null;
       case 'added':
         return `Property '${parent}${key}' was added with value: ${stringify(value)}`;
       case 'changed':
@@ -33,7 +33,6 @@ const plain = (data) => {
       default:
         throw new Error(`Error. Unknown type ${type}!`);
     }
-    return null;
   };
   const result = data.map((item) => iter(item));
   return `${result.join('\n')}`;
