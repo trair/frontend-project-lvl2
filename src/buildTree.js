075f4c00f6dfc5d13aa93b2ad2af6a35c1003173
Maxim Nikonov <the_roffler@mail.ru>
2022-08-24 22:08:42 +0300

    fix

--- src/buildTree.js
+++ src/buildTree.js
@@ -4,14 +4,14 @@ const buildTree = (data1, data2) => {
   const uniqKeys = _.uniq([...Object.keys(data1), ...Object.keys(data2)]);
   const sortKeys = _.sortBy(uniqKeys);
   const result = sortKeys.map((key) => {
-    const value1 = obj1[key];
-    const value2 = obj2[key];
+    const value1 = data1[key];
+    const value2 = data2[key];
 
     if (_.isObject(value1) && _.isObject(value2)) {
       return {
         type: 'object',
         key,
-        children: compareData(value1, value2),
+        children: buildTree(value1, value2),
       };
     }
     if (!_.has(obj2, key)) {
