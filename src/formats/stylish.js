39ca5407ddfee0f9dbf8b75f6c4ed261f758c0b7
Maxim Nikonov <the_roffler@mail.ru>
2022-08-26 11:08:49 +0300

    fix

--- src/formats/stylish.js
+++ src/formats/stylish.js
@@ -1,53 +1,50 @@
 import _ from 'lodash';
 
-const currentIndent = (depth, replacer = ' ', spacesCount = 4) => {
-  const indentSize = depth * spacesCount;
-  return replacer.repeat(indentSize - 1);
-};
+const genIndent = (num, str = ' ') => str.repeat(num * 4 - 2);
 
-const stringify = (value, depth) => {
+const makeString = (value, num = 1) => {
   if (!_.isObject(value)) {
-    return `${value}`;
+    return value;
   }
-
-  const indent = currentIndent(depth);
-  const bracketIndent = currentIndent(depth + 1);
-  const lines = Object
-    .entries(value)
-    .map(([key, val]) => `${bracketIndent.slice(1)}  ${key}: ${stringify(val, depth + 1)}`);
-
-  return [
-    '{',
-    ...lines,
-    `${indent} }`,
-  ].join('\n');
-};
-
-const iter = (tree, depth = 1) => {
-  const result = tree.map((node) => {
-    const indent = currentIndent(depth);
-    switch (node.type) {
-      case 'deleted':
-        return `${indent.slice(1)}- ${node.key}: ${stringify(node.value, depth)}`;
-      case 'added':
-        return `${indent.slice(1)}+ ${node.key}: ${stringify(node.value, depth)}`;
-      case 'unchanged':
-        return `${indent.slice(1)}  ${node.key}: ${stringify(node.value, depth)}`;
-      case 'changed':
-        return [
-          `${indent.slice(1)}- ${node.key}: ${stringify(node.value, depth)}`,
-          `${indent.slice(1)}+ ${node.key}: ${stringify(node.newValue, depth)}`,
-        ];
-      case 'nested':
-        return `${indent} ${node.key}: {\n${iter(node.children, depth + 1)}\n${indent} }`;
-      default:
-        throw new Error('missing selector');
-    }
+  const keys = Object.keys(value);
+  const result = keys.map((key) => {
+    const newKey = value[key];
+    return `${genIndent(num + 1)}  ${key}: ${makeString(newKey, num + 1)}`;
   });
-
-  return _.flatten(result).join('\n');
+  return `{\n${result.join('\n')}\n  ${genIndent(num)}}`;
 };
 
-const stylish = (tree) => `{\n${iter(tree)}\n}`;
-
+const stylish = (data) => {
+  const iter = (node, num = 1) => {
+    const {
+      type,
+      key,
+      value,
+      valueDeleted,
+      valueAdded,
+      children,
+    } = node;
+    const result1 = `${genIndent(num)}- ${key}: ${makeString(valueDeleted, num)}`;
+    const result2 = `${genIndent(num)}+ ${key}: ${makeString(valueAdded, num)}`;
+    switch (type) {
+      case 'nested': {
+        const objectResult = children.flatMap((child) => iter(child, num + 1));
+        return `${genIndent(num)}  ${key}: {\n${objectResult.join('\n')}\n${genIndent(num)}  }`;
+      }
+      case 'deleted':
+        return `${genIndent(num)}- ${key}: ${makeString(value, num)}`;
+      case 'added':
+        return `${genIndent(num)}+ ${key}: ${makeString(value, num)}`;
+      case 'changed':
+        return (`${result1}\n${result2}`);
+      case 'unchanged':
+        return `${genIndent(num)}  ${key}: ${makeString(value, num)}`;
+      default:
+        console.log('Error');
+    }
+    return node;
+  };
+  const result = data.map((item) => iter(item));
+  return `{\n${result.join('\n')}\n}`;
+};
 export default stylish;
