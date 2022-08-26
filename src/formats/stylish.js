3b0f4f04c44f5fdfc01fec963aff78e509dec6df
Maxim Nikonov <the_roffler@mail.ru>
2022-08-26 11:15:02 +0300

    fix

--- src/formats/stylish.js
+++ src/formats/stylish.js
@@ -1,21 +1,21 @@
 import _ from 'lodash';
 
-const genIndent = (num, str = ' ') => str.repeat(num * 4 - 2);
+const genIndent = (depth, str = ' ') => str.repeat(depth * 4 - 2);
 
-const makeString = (value, num = 1) => {
+const makeString = (value, depth = 1) => {
   if (!_.isObject(value)) {
     return value;
   }
   const keys = Object.keys(value);
   const result = keys.map((key) => {
     const newKey = value[key];
-    return `${genIndent(num + 1)}  ${key}: ${makeString(newKey, num + 1)}`;
+    return `${genIndent(depth + 1)}  ${key}: ${makeString(newKey, depth + 1)}`;
   });
-  return `{\n${result.join('\n')}\n  ${genIndent(num)}}`;
+  return `{\n${result.join('\n')}\n  ${genIndent(depth)}}`;
 };
 
 const stylish = (data) => {
-  const iter = (node, num = 1) => {
+  const iter = (node, depth = 1) => {
     const {
       type,
       key,
@@ -24,25 +24,24 @@ const stylish = (data) => {
       valueAdded,
       children,
     } = node;
-    const result1 = `${genIndent(num)}- ${key}: ${makeString(valueDeleted, num)}`;
-    const result2 = `${genIndent(num)}+ ${key}: ${makeString(valueAdded, num)}`;
+    const result1 = `${genIndent(depth)}- ${key}: ${makeString(valueDeleted, depth)}`;
+    const result2 = `${genIndent(depth)}+ ${key}: ${makeString(valueAdded, depth)}`;
     switch (type) {
       case 'nested': {
-        const objectResult = children.flatMap((child) => iter(child, num + 1));
-        return `${genIndent(num)}  ${key}: {\n${objectResult.join('\n')}\n${genIndent(num)}  }`;
+        const objectResult = children.flatMap((child) => iter(child, depth + 1));
+        return `${genIndent(depth)}  ${key}: {\n${objectResult.join('\n')}\n${genIndent(depth)}  }`;
       }
       case 'deleted':
-        return `${genIndent(num)}- ${key}: ${makeString(value, num)}`;
+        return `${genIndent(depth)}- ${key}: ${makeString(value, depth)}`;
       case 'added':
-        return `${genIndent(num)}+ ${key}: ${makeString(value, num)}`;
+        return `${genIndent(depth)}+ ${key}: ${makeString(value, depth)}`;
       case 'changed':
         return (`${result1}\n${result2}`);
       case 'unchanged':
-        return `${genIndent(num)}  ${key}: ${makeString(value, num)}`;
+        return `${genIndent(depth)}  ${key}: ${makeString(value, depth)}`;
       default:
-        console.log('Error');
+        throw new Error(`Unknown type ${type}`);
     }
-    return node;
   };
   const result = data.map((item) => iter(item));
   return `{\n${result.join('\n')}\n}`;
