196cc6124bc87405c773cbf99d03df6a51304c16
Maxim Nikonov <the_roffler@mail.ru>
2022-08-31 13:52:22 +0300

    fix

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -17,8 +17,6 @@ const makeString = (value, depth = 1) => {
 const stylish = (data) => {
   const iter = (node, depth = 1) => {
     const result = node.map((item) => {
-      const result1 = `${genIndent(depth)}- ${item.key}: ${makeString(item.value1, depth)}`;
-      const result2 = `${genIndent(depth)}+ ${item.key}: ${makeString(item.value2, depth)}`;
       switch (item.type) {
         case 'nested': {
           return `${genIndent(depth)}  ${item.key}: {\n${iter(item.children, depth + 1)}\n${genIndent(depth)}  }`;
@@ -28,7 +26,7 @@ const stylish = (data) => {
         case 'added':
           return `${genIndent(depth)}+ ${item.key}: ${makeString(item.value, depth)}`;
         case 'changed':
-          return (`${result1}\n${result2}`);
+          return (`${genIndent(depth)}- ${item.key}: ${makeString(item.value1, depth)}\n{genIndent(depth)}+ ${item.key}: ${makeString(item.value2, depth)}`);
         case 'unchanged':
           return `${genIndent(depth)}  ${item.key}: ${makeString(item.value, depth)}`;
         default:
